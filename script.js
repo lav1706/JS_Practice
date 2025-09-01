ef4bc974b7e841895d89d0d90c41169631b74b45
lav1706 <lavnishraghav00@gmail.com>
2025-09-01 19:20:38 +0530

    2nd Day

--- script.js
+++ script.js
@@ -117,69 +117,88 @@ console.log(countPet);
 //   return acc;
 // }, []);
 // console.log(usersResult);
-const participation = [
-  { name: "Rahul", hours: 6 },
-  { name: "Neha", hours: 4 },
-  { name: "Ankit", hours: 3 },
-  { name: "Rahul", hours: 5 },
-  { name: "Neha", hours: 2 },
-  { name: "Anjali", hours: 9 },
-];
-const filterParticipation = participation.reduce((acc, curr) => {
-  acc[curr.name] = (acc[curr.name] || 0) + curr.hours;
-  return acc;
-}, {});
-const result = Object.fromEntries(
-  Object.entries(filterParticipation).filter(([name, hours]) => hours > 8)
-);
-console.log(result);
-const sales = [
-  { executive: "Rohit", amount: 5000 },
-  { executive: "Sneha", amount: 4000 },
-  { executive: "Rohit", amount: 3000 },
-  { executive: "Sneha", amount: 1000 },
-  { executive: "Arjun", amount: 6000 },
-];
-const salesResult = sales.reduce((acc, curr) => {
-  acc[curr.executive] = (acc[curr.executive] || 0) + curr.amount;
+// const participation = [
+//   { name: "Rahul", hours: 6 },
+//   { name: "Neha", hours: 4 },
+//   { name: "Ankit", hours: 3 },
+//   { name: "Rahul", hours: 5 },
+//   { name: "Neha", hours: 2 },
+//   { name: "Anjali", hours: 9 },
+// ];
+// const filterParticipation = participation.reduce((acc, curr) => {
+//   acc[curr.name] = (acc[curr.name] || 0) + curr.hours;
+//   return acc;
+// }, {});
+// const result = Object.fromEntries(
+//   Object.entries(filterParticipation).filter(([name, hours]) => hours > 8)
+// );
+// console.log(result);
+// const sales = [
+//   { executive: "Rohit", amount: 5000 },
+//   { executive: "Sneha", amount: 4000 },
+//   { executive: "Rohit", amount: 3000 },
+//   { executive: "Sneha", amount: 1000 },
+//   { executive: "Arjun", amount: 6000 },
+// ];
+// const salesResult = sales.reduce((acc, curr) => {
+//   acc[curr.executive] = (acc[curr.executive] || 0) + curr.amount;
 
-  return acc;
-}, {});
-const finalSol = Object.fromEntries(
-  Object.entries(salesResult).filter(([executive, amount]) => amount > 7000)
-);
-console.log(finalSol);
+//   return acc;
+// }, {});
+// const finalSol = Object.fromEntries(
+//   Object.entries(salesResult).filter(([executive, amount]) => amount > 7000)
+// );
+// console.log(finalSol);
 
-const users = [
-  {
-    id: 1,
-    name: "Alice",
-    videos: [
-      { title: "Intro", size: 80, tags: ["tutorial", "beginner"] },
-      { title: "Advanced Tips", size: 120, tags: ["tutorial", "advanced"] },
-    ],
-  },
-  {
-    id: 2,
-    name: "Bob",
-    videos: [
-      { title: "Travel Vlog", size: 200, tags: ["travel", "vlog"] },
-      { title: "Food Tour", size: 150, tags: ["food", "travel"] },
-    ],
-  },
+// const users = [
+//   {
+//     id: 1,
+//     name: "Alice",
+//     videos: [
+//       { title: "Intro", size: 80, tags: ["tutorial", "beginner"] },
+//       { title: "Advanced Tips", size: 120, tags: ["tutorial", "advanced"] },
+//     ],
+//   },
+//   {
+//     id: 2,
+//     name: "Bob",
+//     videos: [
+//       { title: "Travel Vlog", size: 200, tags: ["travel", "vlog"] },
+//       { title: "Food Tour", size: 150, tags: ["food", "travel"] },
+//     ],
+//   },
+// ];
+// const analyzeVideos = (users) => {
+//   const UserStorage = users.reduce((acc, user) => {
+//     const totalSize = user.videos.reduce((acc, video) => acc + video.size, 0);
+//     acc[user.name] = totalSize;
+//     return acc;
+//   }, {});
+//   return UserStorage;
+// };
+// console.log(analyzeVideos(users));
+// const allTags = users.flatMap((user) =>
+//   user.videos.flatMap((video) => video.tags)
+// );
+// const tagSet = new Set(allTags);
+
+// console.log(tagSet);
+
+//
+
+// const users = [
+//   { name: "Alice", email: "alice@email.com" },
+//   { name: "Bob", email: "bob@email.com" },
+// ];
+// const result = users.reduce((acc, curr) => {
+//   acc[curr.name] = curr.email;
+//   return acc;
+// }, {});
+// console.log(result);
+
+const orders = [
+  { id: 1, products: [{ price: 10 }, { price: 20 }] },
+  { id: 2, products: [{ price: 15 }, { price: 5 }] },
 ];
-const analyzeVideos = (users) => {
-  const UserStorage = users.reduce((acc, user) => {
-    const totalSize = user.videos.reduce((acc, video) => acc + video.size, 0);
-    acc[user.name] = totalSize;
-    return acc;
-  }, {});
-  return UserStorage;
-};
-console.log(analyzeVideos(users));
-const allTags = users.flatMap((user) =>
-  user.videos.flatMap((video) => video.tags)
-);
-const tagSet = new Set(allTags);
-
-console.log(tagSet);
+const totalPrice = orders.flatMap((price) => price.products);
+console.log(totalPrice);
